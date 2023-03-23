473d618c937303c4ed4a8e8fb4f97f1f762e1865
yaene <yaene@9968.ch>
2023-03-23 22:16:19 +0800

    change to QMainWindow

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use qt_widgets::{QApplication, QWidget};
+use qt_widgets::{QApplication, QMainWindow};
 
 fn main() {
     QApplication::init(|_| unsafe {
-        let window = QWidget::new_0a();
-        window.resize_2a(500, 500);
-        window.show();
+        let window = QMainWindow::new_0a();
+        window.resize_2a(500, 300);
+        window.show_maximized();
         QApplication::exec()
     })
 }
