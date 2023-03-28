1dcef1bb18b832d0d7e5fed5b8b7a2cdf161115d
yaene <yaene@9968.ch>
2023-03-28 22:49:47 +0800

    add read selectors functionality

--- src/administrator.rs
+++ src/administrator.rs
@@ -1,11 +1,11 @@
 // RUN WITH cargo test -- --nocapture
 // to see output for current integration tests
-// todo yb: make proper unit tests
+// todo yb: make proper unit tests and error handling + cleanup code - lots to do :(
 
 const ADMINISTRATOR_URL: &str = "http://localhost:9090";
 
-pub fn read_proxies() -> Result<String, reqwest::Error> {
-    reqwest::blocking::get(ADMINISTRATOR_URL.to_string() + "/proxies")?.text()
+fn read_proxies() -> Result<reqwest::blocking::Response, reqwest::Error> {
+    reqwest::blocking::get(ADMINISTRATOR_URL.to_string() + "/proxies")
 }
 
 pub fn use_proxy(
@@ -17,6 +17,20 @@ pub fn use_proxy(
         .put(ADMINISTRATOR_URL.to_string() + "/proxies/" + selector)
         .body(format!("{{\"name\":\"{}\"}}", proxy))
         .send()
+}
+pub fn read_selectors() -> Result<Vec<String>, reqwest::Error> {
+    let proxies = &read_proxies()?.json::<serde_json::Value>()?["proxies"];
+
+    let proxies: Option<&serde_json::Map<String, serde_json::Value>> =
+        proxies.as_object();
+
+    Ok(proxies
+        .expect("unexpected response format, check code")
+        .values()
+        .filter(|proxy| proxy["type"] == "Selector")
+        .filter_map(|proxy| proxy["name"].as_str())
+        .map(String::from)
+        .collect::<Vec<String>>())
 }
 
 #[cfg(test)]
@@ -38,10 +52,19 @@ mod tests {
 
     #[test]
     fn test_use_proxy() {
-        match use_proxy(
-            "%F0%9F%90%9F%20%E6%BC%8F%E7%BD%91%E4%B9%8B%E9%B1%BC",
-            "荷兰｜IEPL｜欧洲-2X",
-        ) {
+        match use_proxy("🚀 节点选择", "荷兰｜IEPL｜欧洲-2X") {
+            Ok(resp) => {
+                println!("{:#?}", resp);
+            }
+            Err(err) => {
+                println!("{:#?}", err);
+            }
+        }
+    }
+    #[test]
+
+    fn test_read_selectors() {
+        match read_selectors() {
             Ok(resp) => {
                 println!("{:#?}", resp);
             }
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ mod administrator;
 fn main() {
     QApplication::init(|_| unsafe {
         let window = QMainWindow::new_0a();
-        let proxies = administrator::read_proxies();
-        let label =
-            QLabel::from_q_string_q_widget(&qs(proxies.unwrap()), &window);
+        let proxies = administrator::read_selectors();
+        let label = QLabel::from_q_string_q_widget(
+            &qs(proxies.unwrap().concat()),
+            &window,
+        );
         label.set_fixed_width(500);
         label.set_word_wrap(true);
         window.resize_2a(500, 300);
