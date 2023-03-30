664b488f5bba454f48e5da6528c4f05165bb179b
yaene <yaene@9968.ch>
2023-03-30 21:16:22 +0800

    show selectors in ListWidget

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use qt_core::qs;
-use qt_widgets::{QApplication, QLabel, QMainWindow};
+use qt_core::{cpp_core::CppBox, qs, QListOfQString, QString, QStringList};
+use qt_widgets::{QApplication, QLabel, QListWidget, QMainWindow};
 
 mod administrator;
 
@@ -7,14 +7,19 @@ fn main() {
     QApplication::init(|_| unsafe {
         let window = QMainWindow::new_0a();
         let proxies = administrator::read_selectors();
-        let label = QLabel::from_q_string_q_widget(
-            &qs(proxies.unwrap().concat()),
-            &window,
-        );
-        label.set_fixed_width(500);
-        label.set_word_wrap(true);
+        let selector_select = QListWidget::new_1a(&window);
+        selector_select.add_items(&to_qstring_list(&proxies.unwrap()));
+        selector_select.resize_2a(500, 300);
         window.resize_2a(500, 300);
         window.show_maximized();
         QApplication::exec()
     })
+}
+
+unsafe fn to_qstring_list(input: &Vec<String>) -> CppBox<QStringList> {
+    let list = QStringList::new();
+    for str in input.iter() {
+        list.append_q_string(&qs(str));
+    }
+    list
 }
