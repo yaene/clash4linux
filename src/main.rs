07ed3312dc23ea7ba39454cdb1720d085341cfb7
yaene <yaene@9968.ch>
2023-03-30 23:13:14 +0800

    add show proxies for selector

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,82 @@
-use qt_core::{cpp_core::CppBox, qs, QListOfQString, QString, QStringList};
-use qt_widgets::{QApplication, QLabel, QListWidget, QMainWindow};
+use std::rc::Rc;
+
+use cpp_core::{Ptr, StaticUpcast};
+use qt_core::{
+    cpp_core::CppBox, qs, slot, QBox, QListOfQString, QObject, QPtr, QString,
+    QStringList, SlotNoArgs,
+};
+use qt_widgets::{QApplication, QLabel, QListView, QListWidget, QMainWindow};
 
 mod administrator;
 
 fn main() {
     QApplication::init(|_| unsafe {
-        let window = QMainWindow::new_0a();
-        let proxies = administrator::read_selectors();
-        let selector_select = QListWidget::new_1a(&window);
-        selector_select.add_items(&to_qstring_list(&proxies.unwrap()));
-        selector_select.resize_2a(500, 300);
-        window.resize_2a(500, 300);
-        window.show_maximized();
+        let proxy_widget = ProxyWidget::new();
+        proxy_widget.show();
         QApplication::exec()
     })
+}
+struct ProxyWidget {
+    window: QBox<QMainWindow>,
+    selector: QBox<QListWidget>,
+    proxies: QBox<QListWidget>,
+}
+
+impl StaticUpcast<QObject> for ProxyWidget {
+    unsafe fn static_upcast(ptr: Ptr<Self>) -> Ptr<QObject> {
+        ptr.window.as_ptr().static_upcast()
+    }
+}
+
+impl ProxyWidget {
+    fn new() -> Rc<Self> {
+        unsafe {
+            let window = QMainWindow::new_0a();
+            let selector = QListWidget::new_1a(&window);
+            let proxies = QListWidget::new_1a(&window);
+            let this = Rc::new(ProxyWidget {
+                window,
+                selector,
+                proxies,
+            });
+            this.init();
+            this
+        }
+    }
+    unsafe fn init(self: &Rc<Self>) {
+        self.selector.add_items(&to_qstring_list(
+            &administrator::read_selectors().unwrap(),
+        ));
+        self.selector
+            .current_item_changed()
+            .connect(&self.slot_on_selector_selection_changed());
+        // todo yb: error handling when no selectors
+        self.selector.set_current_row_1a(0);
+        self.selector.resize_2a(500, 300);
+        self.proxies.add_items(&to_qstring_list(
+            &administrator::read_proxies_for_selector(
+                &self.selector.current_item().text().to_std_string(),
+            )
+            .unwrap(),
+        ));
+        self.proxies.resize_2a(500, 300);
+        self.proxies.move_2a(0, 300);
+    }
+
+    unsafe fn show(self: &Self) {
+        self.window.show_maximized();
+    }
+
+    #[slot(SlotNoArgs)]
+    unsafe fn on_selector_selection_changed(self: &Self) {
+        self.proxies.clear();
+        self.proxies.add_items(&to_qstring_list(
+            &administrator::read_proxies_for_selector(
+                &self.selector.current_item().text().to_std_string(),
+            )
+            .unwrap(),
+        ));
+    }
 }
 
 unsafe fn to_qstring_list(input: &Vec<String>) -> CppBox<QStringList> {
